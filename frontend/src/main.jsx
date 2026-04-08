10c93ec59bcf2231262d01847ebd2a81813c75db
ardaj <kolostest@email.cz>
2026-04-08 06:17:20 +0200

    oprava mapy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+
+// Načtení tvých aktuálních stylů
 import './index.css'
+import './App.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
