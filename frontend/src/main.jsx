0fbf02370dff5955ea45adec5453d8640223494f
ardaj-ops <jaroslav.stepanec@gmail.com>
2026-04-16 04:26:01 +0000

    oprava

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
 // Načtení tvých aktuálních stylů
-import './index.css'
-import './App.css'
+import './index.css';
+import './App.css';
 
-import App from './App.jsx'
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <App />
-)
->>>>>>> dadd4ccc79dda0e8cb86d54474321d7743fa2076
+);
